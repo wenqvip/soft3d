67333e674cc6f15b5488d2419c1ce548307880ee
wenqvip <wenqvip@gmail.com>
2015-10-05 18:00:30 +0800

    -modify

--- soft3d/soft3d.cpp
+++ soft3d/soft3d.cpp
@@ -1,51 +1,28 @@
-// soft3d.cpp : 定义应用程序的入口点。
-//
-
 #include "stdafx.h"
 #include "soft3d.h"
-#include <d2d1.h>
-#include <d2d1_1.h>
-#include <d3d.h>
-#include <d3d11.h>
-#include <dxgi.h>
-#include <dxgi1_3.h>
-#include <wrl.h>
-#include <exception>
-#pragma comment(lib, "d2d1.lib")
-#pragma comment(lib, "d3d11.lib")
 
 using namespace Microsoft::WRL;
 
-#define MAX_LOADSTRING 100
+namespace soft3d {
 
-// 全局变量: 
-HWND hWnd;
-HINSTANCE hInst;                                // 当前实例
-WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
-WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
-
-// 此代码模块中包含的函数的前向声明: 
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
-ID2D1Factory1* m_d2dFactory;
-ID2D1Device* m_d2dDevice;
-ID2D1DeviceContext* m_d2dContext;
-D3D_FEATURE_LEVEL m_featureLevel;
-IDXGISwapChain1* m_swapChain;
-ComPtr<ID2D1Bitmap1> m_d2dTargetBitmap;
-
-inline void ThrowIfFailed(HRESULT hr)
+soft3dEngine::soft3dEngine(HWND hwnd, uint16 width, uint16 height)
 {
-	if (FAILED(hr))
-	{
-		throw "Error";
-	}
+	m_hWnd = hwnd;
+	m_width = width;
+	m_height = height;
+	m_operateBuffer = new unsigned int[m_width * m_height];
+	InitD2D();
 }
 
-void InitD2D()
+
+soft3dEngine::~soft3dEngine()
+{
+	delete[] m_operateBuffer;
+	ReleaseD2D();
+}
+
+
+void soft3dEngine::InitD2D()
 {
 	D2D1_FACTORY_OPTIONS options;
 	options.debugLevel = D2D1_DEBUG_LEVEL_INFORMATION;
@@ -94,7 +71,7 @@ void InitD2D()
 	ThrowIfFailed(dxgiAdapter->GetParent(IID_PPV_ARGS(&dxgiFactory)));
 
 	// Get the final swap chain for this window from the DXGI factory.
-	ThrowIfFailed(dxgiFactory->CreateSwapChainForHwnd(device.Get(), hWnd, &swapChainDesc, nullptr, nullptr, &m_swapChain));
+	ThrowIfFailed(dxgiFactory->CreateSwapChainForHwnd(device.Get(), m_hWnd, &swapChainDesc, nullptr, nullptr, &m_swapChain));
 
 	// Ensure that DXGI doesn't queue more than one frame at a time.
 	ThrowIfFailed(dxgiDevice->SetMaximumFrameLatency(1));
@@ -126,29 +103,27 @@ void InitD2D()
 	ThrowIfFailed(m_d2dContext->CreateBitmapFromDxgiSurface(dxgiBackBuffer.Get(), &bitmapProperties, &m_d2dTargetBitmap));
 
 	// Now we can set the Direct2D render target.
-	m_d2dContext->SetTarget(m_d2dTargetBitmap.Get());
+	m_d2dContext->SetTarget(m_d2dTargetBitmap);
 }
 
-void ReleaseD2D()
+void soft3dEngine::ReleaseD2D()
 {
 	m_d2dContext->Release();
 	m_d2dDevice->Release();
 	m_d2dFactory->Release();
+	m_d2dTargetBitmap->Release();
 }
 
-void Display()
+void soft3dEngine::Display()
 {
 	if (m_d2dContext == NULL)
 		return;
-	ComPtr<ID2D1SolidColorBrush> pBlackBrush;
-	ThrowIfFailed(m_d2dContext->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::Green), &pBlackBrush));
 
 	m_d2dContext->BeginDraw();
-	RECT rc;
-	GetClientRect(hWnd, &rc);
-	D2D1_RECT_F rectf = D2D1::RectF(rc.left + 100.0f, rc.top + 100.0f, rc.right - 100.0f, rc.bottom - 100.0f);
 
-	m_d2dContext->DrawRectangle(rectf, pBlackBrush.Get());
+	D2D1_RECT_U rect = { 0, 0, m_width, m_height };
+	DrawPixel(100, 100, 0xff00ff, 50);
+	ThrowIfFailed(m_d2dTargetBitmap->CopyFromMemory(&rect, m_operateBuffer, m_width * 4));
 
 	ThrowIfFailed(m_d2dContext->EndDraw());
 
@@ -160,169 +135,33 @@ void Display()
 	ThrowIfFailed(m_swapChain->Present1(1, 0, &parameters));
 }
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+int soft3dEngine::DrawPixel(uint16 x, uint16 y, uint32 value, uint16 size)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	if (x > m_width || y > m_height)
+		return -1;
 
-    // TODO: 在此放置代码。
 
-    // 初始化全局字符串
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_SOFT3D, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
-
-    // 执行应用程序初始化: 
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
-
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_SOFT3D));
-
-    MSG msg;
-
-	InitD2D();
-	Display();
-
-    // 主消息循环: 
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
-
-	ReleaseD2D();
-
-    return (int) msg.wParam;
+	for (uint16 i = 0; i < size; i++)
+	{
+		if (i % 2 == 1)
+		{
+			for (uint16 j = 0; j < size; j++)
+				SetOperateBuffer((x+j) * m_width + y + (i+1)/2, value);
+		}
+		else
+		{
+			for (uint16 j = 0; j < size; j++)
+				SetOperateBuffer((x+j) * m_width + y - i/2, value);
+		}
+	}
+	return 0;
 }
 
-
-
-//
-//  函数: MyRegisterClass()
-//
-//  目的: 注册窗口类。
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
+void soft3dEngine::SetOperateBuffer(uint32 index, uint32 value)
 {
-    WNDCLASSEXW wcex;
-
-    wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SOFT3D));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_SOFT3D);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-    return RegisterClassExW(&wcex);
+	if (index >= m_width * m_height)
+		return;
+	m_operateBuffer[index] = value;
 }
 
-//
-//   函数: InitInstance(HINSTANCE, int)
-//
-//   目的: 保存实例句柄并创建主窗口
-//
-//   注释: 
-//
-//        在此函数中，我们在全局变量中保存实例句柄并
-//        创建和显示主程序窗口。
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   hInst = hInstance; // 将实例句柄存储在全局变量中
-
-   hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
-      CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, nullptr, nullptr, hInstance, nullptr);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
-}
-
-//
-//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  目的:    处理主窗口的消息。
-//
-//  WM_COMMAND  - 处理应用程序菜单
-//  WM_PAINT    - 绘制主窗口
-//  WM_DESTROY  - 发送退出消息并返回
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // 分析菜单选择: 
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: 在此处添加使用 hdc 的任何绘图代码...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
-}
-
-// “关于”框的消息处理程序。
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
 }
--- soft3d/soft3d.h
+++ soft3d/soft3d.h
@@ -1,3 +1,57 @@
 #pragma once
+#include <d2d1.h>
+#include <d2d1_1.h>
+#include <d3d.h>
+#include <d3d11.h>
+#include <dxgi.h>
+#include <dxgi1_3.h>
+#include <wrl.h>
+#include <exception>
+#pragma comment(lib, "d2d1.lib")
+#pragma comment(lib, "d3d11.lib")
 
-#include "resource.h"
+namespace soft3d {
+
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+
+class soft3dEngine
+{
+public:
+	soft3dEngine(HWND hwnd, uint16 width, uint16 height);
+	~soft3dEngine();
+
+	void Display();
+	int DrawPixel(uint16 x, uint16 y, uint32 value, uint16 size);
+
+protected:
+	void InitD2D();
+	void ReleaseD2D();
+
+private:
+	inline void ThrowIfFailed(HRESULT hr) {
+		if (FAILED(hr))
+		{
+			throw "Error";
+		}
+	}
+
+	void SetOperateBuffer(uint32 index, uint32 value);
+
+private:
+	HWND m_hWnd;
+	uint16 m_width;
+	uint16 m_height;
+
+	ID2D1Factory1* m_d2dFactory;
+	ID2D1Device* m_d2dDevice;
+	ID2D1DeviceContext* m_d2dContext;
+	D3D_FEATURE_LEVEL m_featureLevel;
+	IDXGISwapChain1* m_swapChain;
+	ID2D1Bitmap1* m_d2dTargetBitmap;
+
+	unsigned int* m_operateBuffer;
+};
+
+}
+
